c882c5bda73112767affe524c3b557538fef8287
lydat143 <lydat143@gmail.com>
2017-11-26 16:25:52 +0700

    update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,9 +1,37 @@
-create table companies (
-  id int NOT NULL PRIMARY KEY ,
+CREATE TABLE  company (
+  id INT NOT NULL PRIMARY KEY ,
+  idJob INT[] REFERENCES job(id),
+  name TEXT,
+  address TEXT,
+  country TEXT
+);
 
-)
+CREATE TABLE  job (
+  id INT NOT NULL PRIMARY KEY,
+  title TEXT,
+  salary MONEY,
+  address TEXT,
+  time_posted TIME,
+  reason TEXT,
+  description TEXT,
+  skill TEXT,
+  qualification TEXT
+);
 
-create table company (
---   id
+CREATE TABLE programming_language(
+  id INT NOT NULL PRIMARY KEY ,
+  idJob INT[] REFERENCES job(id),
+  name text
+);
 
-)
+CREATE TABLE user(
+  id INT NOT NULL PRIMARY KEY ,
+  idPLangs INT[] REFERENCES programming_language(id),
+  idCompanies INT[] REFERENCES company(id),
+  name TEXT,
+  email TEXT,
+  username TEXT,
+  passwd TEXT,
+  address TEXT,
+  phone TEXT
+);
