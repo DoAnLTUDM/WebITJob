75a02000288631777ec43657864717f74984baa5
Giang, Thai Nguyen Minh <14520231@gm.uit.edu.vn>
2017-11-26 23:05:15 +0700

    update func getCompaniesByUrl

--- schedule_crawl_data.go
+++ schedule_crawl_data.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"github.com/robfig/cron"
-	"log"
+    "github.com/robfig/cron"
+    "log"
 )
 
 func schedule_crawl_data(){
-	c := cron.New()
-	c.AddFunc("@daily", func() {
-		crawl_data("https://itviec.com/vi")
-		crawl_data("https://www.vietnamworks.com/viec-lam-it-phan-cung-mang-i55-vn")
-		log.Println("Daily crawing data")
-	})
-	c.Start()
+    c := cron.New()
+    c.AddFunc("@daily", func() {
+        crawl_data("https://itviec.com/vi")
+        crawl_data("https://www.vietnamworks.com/viec-lam-it-phan-cung-mang-i55-vn")
+        log.Println("Daily crawing data")
+    })
+    c.Start()
 }
