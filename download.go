ddffc3bff58845eddd5b1082ffe5850ad9f8e314
Giang, Thai Nguyen Minh <giang.tnm@komit-consulting.com>
2017-11-30 00:44:55 +0700

    fix_code_style

--- download.go
+++ download.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
-	"os"
+    "io"
+    "log"
+    "net/http"
+    "os"
 )
 
 var bytesToKiloBytes = 1024.0
@@ -12,39 +12,39 @@ var bytesToKiloBytes = 1024.0
 // PassThru code originally from
 // http://stackoverflow.com/a/22422650/613575
 type PassThru struct {
-	io.Reader
-	curr  int64
-	total float64
+    io.Reader
+    curr  int64
+    total float64
 }
 
 func (pt *PassThru) Read(p []byte) (int, error) {
-	n, err := pt.Reader.Read(p)
-	pt.curr += int64(n)
+    n, err := pt.Reader.Read(p)
+    pt.curr += int64(n)
 
-	// last read will have EOF err
-	if err == nil || (err == io.EOF && n > 0) {
-		log.Println("Start download image")
-	}
+    // last read will have EOF err
+    if err == nil || (err == io.EOF && n > 0) {
+        log.Println("Start download image")
+    }
 
-	return n, err
+    return n, err
 }
 
 func download(url string, locat string, file_name string) {
-	if _, err := os.Stat(locat+file_name); os.IsNotExist(err){
-		resp, _ := http.Get(url)
-		defer resp.Body.Close()
+    if _, err := os.Stat(locat+file_name); os.IsNotExist(err){
+        resp, _ := http.Get(url)
+        defer resp.Body.Close()
 
-		out, _ := os.Create(locat+file_name)
-		defer out.Close()
+        out, _ := os.Create(locat+file_name)
+        defer out.Close()
 
-		src := &PassThru{Reader: resp.Body, total: float64(resp.ContentLength)}
+        src := &PassThru{Reader: resp.Body, total: float64(resp.ContentLength)}
 
-		size, err := io.Copy(out, src)
-		if err != nil {
-			log.Println(err)
-			return
-		}
+        size, err := io.Copy(out, src)
+        if err != nil {
+            log.Println(err)
+            return
+        }
 
-		log.Printf("Download success %s (%.1f KB)\n",file_name, float64(size)/bytesToKiloBytes)
-	}
+        log.Printf("Download success %s (%.1f KB)\n",file_name, float64(size)/bytesToKiloBytes)
+    }
 }
